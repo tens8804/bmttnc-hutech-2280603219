1e876f1b0107197d307c1e6d9fc2ddbf7772025c
tens8804 <tien882004@gmail.com>
2025-04-03 13:34:28 +0700

    lab02-04

--- lab-03/vigenere_cipher.py
+++ lab-03/vigenere_cipher.py
@@ -1,27 +1,32 @@
 import sys
-import requests
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from ui.vigenere import Ui_MainWindow
+from ui.vigenere import Ui_MainWindow  # Đảm bảo rằng bạn đã chuyển file .ui sang .py
+import requests
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
-        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+        self.ui = Ui_MainWindow()  # Khởi tạo Ui_MainWindow
+        self.ui.setupUi(self)  # Thiết lập giao diện
+
+        # Sử dụng đúng tên các nút (theo tên trong file UI)
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)  # Đảm bảo rằng nút có tên encrypt_button
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)  # Đảm bảo rằng nút có tên decrypt_button
 
     def call_api_encrypt(self):
         url = "http://127.0.0.1:5000/api/vigenere/encrypt"
+        
         payload = {
             "plain_text": self.ui.txt_plain_text.toPlainText(),
             "key": self.ui.txt_key.toPlainText()
         }
+
         try:
             response = requests.post(url, json=payload)
+            
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_cipher_text.setPlainText(data["encrypted_message"])
+                self.ui.txt_cipher_text.setPlainText(data["encrypted_text"])
                 
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
@@ -30,19 +35,22 @@ class MyApp(QMainWindow):
             else:
                 print("Error while calling API")
         except requests.exceptions.RequestException as e:
-            print("Error: %s" % e)
+            print(f"Error: {e}")
 
     def call_api_decrypt(self):
         url = "http://127.0.0.1:5000/api/vigenere/decrypt"
+        
         payload = {
             "cipher_text": self.ui.txt_cipher_text.toPlainText(),
             "key": self.ui.txt_key.toPlainText()
         }
+
         try:
             response = requests.post(url, json=payload)
+            
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_plain_text.setPlainText(data["decrypted_message"])
+                self.ui.txt_plain_text.setPlainText(data["decrypted_text"])
                 
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
@@ -51,7 +59,7 @@ class MyApp(QMainWindow):
             else:
                 print("Error while calling API")
         except requests.exceptions.RequestException as e:
-            print("Error: %s" % e)
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
